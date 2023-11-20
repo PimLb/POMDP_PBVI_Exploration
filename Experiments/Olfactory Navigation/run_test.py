c7703325b62aa5cf29c2d1618526005d2fab56e6
thebot002 <arnaudr85@gmail.com>
2023-11-20 18:35:45 +0100

    More experiments on the phenomenon of poor perf for ground only sniff

--- Experiments/Olfactory Navigation/run_test.py
+++ Experiments/Olfactory Navigation/run_test.py
@@ -8,7 +8,6 @@ import numpy as np
 import cupy as cp
 import json
 import os
-import multiprocessing
 
 from cupy.cuda import runtime as cuda_runtime
 from csv import writer as csv_writer
@@ -18,24 +17,23 @@ def reward_func(s,a,sn,o):
     return np.where(sn == 10890, 1.0, 0.0)
 
 def main():
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-
-    TEST_COUNT = 100
-    MODEL_FILE = './Models/Alt_Wrap_GroundAir.pck'
-    SIM_FOLDER = f'{timestamp}_test_simulations_ground_air'
-    EXTRA_STEP_FILE = f'./{timestamp}-extra_steps_ground_air.csv'
-
     # Set GPU used
     cuda_runtime.setDevice(1)
-    multiprocessing.set_start_method('spawn', force=True)
 
-    # Creating simulation dir
-    os.mkdir(SIM_FOLDER)
+    # timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    # Create extra steps file
-    with open(EXTRA_STEP_FILE, 'w') as f_object:
-        writer_object = csv_writer(f_object)
-        writer_object.writerow(['Average'] + [f'Sim-{i}' for i in range(300)])
+    # TEST_COUNT = 100
+    # MODEL_FILE = './Models/Alt_Wrap_GroundAir.pck'
+    # SIM_FOLDER = f'{timestamp}_test_simulations_ground_air'
+    # EXTRA_STEP_FILE = f'./{timestamp}-extra_steps_ground_air.csv'
+
+    # # Creating simulation dir
+    # os.mkdir(SIM_FOLDER)
+
+    # # Create extra steps file
+    # with open(EXTRA_STEP_FILE, 'w') as f_object:
+    #     writer_object = csv_writer(f_object)
+    #     writer_object.writerow(['Average'] + [f'Sim-{i}' for i in range(300)])
 
 
     def run(iter):
@@ -69,45 +67,43 @@ def main():
             (361*45)+300  # Bellow center
         ])
 
+        # Computing extra steps
+        print()
+        all_length = [len(sim) for sim in all_sim_histories]
+        opt_traj = np.ones(300) * 240
+        opt_traj[100:] += 15
+
+        extra_steps = (np.array(all_length) - opt_traj)
+        log(f'Average Extra steps count: {np.average(extra_steps)}')
+
+        # Saving extra step results
+        with open(EXTRA_STEP_FILE, 'a') as f_object:
+            writer_object = csv_writer(f_object)
+            writer_object.writerow([np.average(extra_steps)] + extra_steps.tolist())
+        
         # Saving simulations
         log('Saving results')
-        all_length = [len(sim) for sim in all_sim_histories]
-        max_length = max(all_length)
-        all_cols = {}
+        all_seq = np.empty((300,1001), dtype=object)
         for sim_i, sim in enumerate(all_sim_histories):
             seq = []
             for s, a, o, r in zip(sim.states, sim.actions+[], sim.observations+[], sim.rewards+[]):
                 seq.append(json.dumps({'s':s, 'a':a, 'o':o, 'r':r}))
             
-            all_cols[f'Sim-{sim_i}'] = seq + ((1001 - len(sim)) * [None])
+            all_seq[sim_i, :len(seq)] = seq
 
-        sim_df = pd.DataFrame(all_cols)
+        sim_df = pd.DataFrame(all_seq.T, columns=[f'Sim-{sim_i}' for sim_i in range(len(all_sim_histories))])
         sim_df.to_csv(f'./{SIM_FOLDER}/run-{iter}-sims.csv')
-
-        # Processing results
-        opt_traj = np.ones(300) * 240
-        opt_traj[100:] += 15
-
-        extra_steps = (np.array(all_length) - opt_traj)
-        
-
-        with open(EXTRA_STEP_FILE, 'a') as f_object:
-            writer_object = csv_writer(f_object)
-            writer_object.writerow([np.average(extra_steps)] + extra_steps.tolist())
-        
-        print()
-        log(f'Results: {np.average(extra_steps)}')
 
         print('\n\n')
 
-    # Actual test loop
-    for i in range(TEST_COUNT):
-        try:
-            run(i)
-        except:
-            print()
-            log('/!\\ Error Happend /!\\ \n\n')
-        cp._default_memory_pool.free_all_blocks()
+    # # Actual test loop
+    # for i in range(TEST_COUNT):
+    #     try:
+    #         run(i)
+    #     except:
+    #         print()
+    #         log('/!\\ Error Happend /!\\ \n\n')
+    #     cp._default_memory_pool.free_all_blocks()
 
     '''
     ----------------------------------------------------------------------------------------------------------------
@@ -116,14 +112,10 @@ def main():
     '''
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    TEST_COUNT = 100
+    TEST_COUNT = 20
     MODEL_FILE = './Models/Alt_Wrap_GroundOnly.pck'
     SIM_FOLDER = f'{timestamp}_test_simulations_ground_only'
     EXTRA_STEP_FILE = f'./{timestamp}-extra_steps_ground_only.csv'
-
-    # Set GPU used
-    cuda_runtime.setDevice(1)
-    multiprocessing.set_start_method('spawn', force=True)
 
     # Creating simulation dir
     os.mkdir(SIM_FOLDER)
@@ -137,10 +129,16 @@ def main():
     for i in range(TEST_COUNT):
         try:
             run(i)
-        except:
+        except Exception as e:
             print()
+            print(e)
             log('/!\\ Error Happend /!\\ \n\n')
         cp._default_memory_pool.free_all_blocks()
+    
+    print('\n\n')
+    print('--------------------------------------------------------------------------------')
+    print(f'DONE')
+    print('--------------------------------------------------------------------------------')
 
 
 if __name__ == "__main__":
